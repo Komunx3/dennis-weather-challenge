92933c842fb5828c041dd952f70586605dc27120
Komunx3 <37865126+Komunx3@users.noreply.github.com>
2021-07-30 20:19:12 +0200

    feat(JsonReader, WebServiceReader): Added obligatory additional Readers to visualize extension possibility

---
+++ src/main/java/de/exxcellent/challenge/reader/WebServiceReader.java
@@ -0,0 +1,17 @@
+package de.exxcellent.challenge.reader;
+
+import java.util.HashMap;
+import java.util.List;
+
+import de.exxcellent.challenge.exceptions.DataNotAvailableException;
+import de.exxcellent.challenge.exceptions.IllegalFormatException;
+
+public class WebServiceReader implements Reader {
+
+    @Override
+    public List<HashMap<String, String>> getData() throws IllegalFormatException, DataNotAvailableException {
+       /* No Task in this challenge, but possible to build */
+       return null;
+    }
+
+}
