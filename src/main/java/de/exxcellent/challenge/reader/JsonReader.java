c9fc4378607e5b06c2f8db68dcae966fd8f394c7
Komunx3 <37865126+Komunx3@users.noreply.github.com>
2021-07-31 22:06:27 +0200

    refactor(all): Overall refactoring

--- src/main/java/de/exxcellent/challenge/reader/JsonReader.java
+++ src/main/java/de/exxcellent/challenge/reader/JsonReader.java
@@ -1,7 +1,5 @@
 package de.exxcellent.challenge.reader;
 
-import de.exxcellent.challenge.exceptions.DataNotAvailableException;
-import de.exxcellent.challenge.exceptions.IllegalFormatException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class JsonReader extends Reader {
@@ -16,7 +14,7 @@ public class JsonReader extends Reader {
     }
 
     @Override
-    protected void updateData() throws IllegalFormatException, DataNotAvailableException {
+    protected void updateData(){
         /*
         No Task in this challenge, but possible to build with the external library "JFlat".
         This library can convert a json data structure to a flat data structure like csv.
--- src/main/java/de/exxcellent/challenge/reader/WebServiceReader.java
+++ src/main/java/de/exxcellent/challenge/reader/WebServiceReader.java
@@ -1,10 +1,5 @@
 package de.exxcellent.challenge.reader;
 
-import java.util.HashMap;
-import java.util.List;
-
-import de.exxcellent.challenge.exceptions.DataNotAvailableException;
-import de.exxcellent.challenge.exceptions.IllegalFormatException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class WebServiceReader extends Reader {
